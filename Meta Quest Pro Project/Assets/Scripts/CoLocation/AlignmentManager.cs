0f68704ebe18326e381e1856d349c155cda176b3
Bora Ciftci <90639949+borciftci@users.noreply.github.com>
2025-02-11 14:06:49 +0100

    Update AlignmentManager.cs
    Filled in AlignmentManager.cs

--- Meta Quest Pro Project/Assets/Scripts/CoLocation/AlignmentManager.cs
+++ Meta Quest Pro Project/Assets/Scripts/CoLocation/AlignmentManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class AlignmentManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform _cameraRigTransform;
+
+    public void Awake() 
     {
-        
+        _cameraRigTransform = FindAnyObjectByType<OVRCameraRig>().transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AlignUserToAnchor(OVRSpatialAnchor anchor) 
     {
+        if (!anchor || !anchor.Localized)
+        {
+            Debug.LogError("Colocation: Invalid or unlocalized anchor. Cannot align.");
+            return;
+        }
         
+        Debug.Log($"Colocation: Aligning user to anchor {anchor.Uuid}.");
+
+        StartCoroutine(AlignmentCoroutine(anchor));
+    }
+
+    private IEnumerator AlignmentCoroutine(OVRSpatialAnchor anchor) 
+    {
+        var anchorTransform = anchor.transform;
+
+        for (var alignmentCount = 2; alignmentCount > 0; alignmentCount--) 
+        {
+            _cameraRigTransform.position = Vector3.zero;
+            _cameraRigTransform.eulerAngles = Vector3.zero;
+
+            yield return null;
+
+            _cameraRigTransform.position = anchorTransform.InverseTransformPoint(Vector3.zero);
+            _cameraRigTransform.eulerAngles = new Vector3(0, -anchorTransform.eulerAngles.y, 0);
+            Debug.Log($"Colocation: Aligned camera rig position: {_cameraRigTransform.position}");
+
+            yield return new WaitForEndOfFrame();
+
+        }
     }
 }
+
